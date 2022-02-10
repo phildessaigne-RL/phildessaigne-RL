2dd9ced8b59a6181c9fa69d81577fa3df83164af
Phillipe Dessaigne <phillipe.dessaigne@phillipes-mbp.lan>
2022-02-10 12:13:27 -0500

    excercise2 commit

--- excercise2/main.py
+++ excercise2/main.py
@@ -18,7 +18,7 @@ import requests
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
-PROGRAM_NAME = "excercise1"
+PROGRAM_NAME = "excercise2"
 if not os.path.exists('output/' + PROGRAM_NAME):
     os.mkdir('output/' + PROGRAM_NAME)
 
@@ -64,20 +64,131 @@ def load_config(name):
         except yaml.YAMLError as exc:
             print(exc)
 
+def get_port_group_id_from_name(port_group_name):
+    port_groups_xref = get_port_groups_xref()
+    port_group_id = port_groups_xref.loc[port_groups_xref['group_name'] == port_group_name, 'group_id'].iloc[0]
+    return port_group_id
+
+
+def get_port_groups_xref():
+    rl_resp = make_api_call_json("FetchFullPortfolioTrees", {}, API_URL, API_KEY)
+    edges = rl_resp['data']['portfolioTrees']['edges']
+    trees = [(e['node']['tree']) for e in edges]
+
+    df_groups = pd.DataFrame()
+    for x, tree in enumerate(trees):
+        tree = json.loads(tree)
+        df_groups = parse_trees(tree, df_groups)
+    df_groups = df_groups.transpose()
+    return df_groups
+
+
+def parse_trees(node, df_groups):
+    if isinstance(node, dict):
+        if 'root' in node:
+            node = node['root']
+
+        if 'children' in node:
+            node = node['children']
+            df_groups = parse_trees(node, df_groups)
+        elif ('id' and 'name' in node) and ('children' not in node):
+            data = {'group_name': node['name'], 'group_id': node['id']}
+            df_groups = pd.concat([df_groups, pd.DataFrame.from_dict(data, orient='index')], axis=1,
+                                  ignore_index=True)
+            return df_groups
+        else:
+            for item in node.items():
+                if isinstance(item[1], dict):
+                    df_groups = parse_trees(item[1], df_groups)
+
+    return df_groups
+
+def launch_holdings_report(as_of_date, portfolio_id):
+    launch_holdings_report_variables = {
+        'portfolioEntities': [portfolio_id],
+        'asOfDate': as_of_date,
+        'columnsSelector': [
+            {"columnName": "PortfolioId"},
+            {'columnName': "Quantity"},
+            {'columnName': 'SecurityType'},
+            {'columnName': "SecurityDescription"},
+            {'columnName': "SecurityMarketPrice"},
+            {'columnName': "MarketValue"},
+            {"columnName": "SecurityCusip"},
+        ],
+    }
+    launch_holdings_report_response = make_api_call_json("LaunchHoldingsReport",
+                                                         launch_holdings_report_variables, API_URL,
+                                                         API_KEY)
+    jobId = launch_holdings_report_response["data"]["launchAccountingReportResult"]["jobId"]
+    logger.debug(f'LaunchHoldingsReport - JobId: {jobId}')
+    return jobId
+
+
+def get_export_data_set(job_id):
+
+    result_json = fetch_export_dataset_response(job_id)
+    result_json_df = pd.read_json(result_json)
+    return result_json_df
+
+
+def fetch_export_dataset_response(job_id):
+    result_json_text = {}
+    fetch_export_dataset_response_variables = {
+        'jobId': job_id
+    }
+
+    # pool the api till success or failure
+
+    try:
+        poll_flag = True
+        while poll_flag:
+            job_response = make_api_call_json("FetchExportDataSetResponse", fetch_export_dataset_response_variables,
+                                              API_URL, API_KEY)
+            if job_response['data']['result']['jobStatus'] == "SUCCEEDED":
+                jobResultsURL = job_response['data']['result']['presignedUrl']
+                file = requests.get(jobResultsURL)
+                result_json_text = file.text
+                poll_flag = False
+            elif job_response['data']['result']['jobStatus'] == "FAILED":
+                logger.error(f'Job Failed : ,{job_id}, with reason: , {job_response["data"]["result"]["failureReason"]}')
+                poll_flag = False
+                exit()
+            else:
+                time.sleep(2)
+
+    except Exception as error:
+        print(error)
+        sys.exit(0)
+    return result_json_text
+
+def sort_lines_by_security_type(json_result_df):
+
+    column_names = ["MarketValue", "PortfolioId","Quantity", "SecurityCusip", "SecurityDescription", "SecurityMarketPrice", "SecurityType"]
+    securities = pd.DataFrame(columns=column_names)
+    cash_equivalencies = pd.DataFrame(columns=column_names)
+    for index, line_item in json_result_df.iterrows():
+        if line_item['SecurityType'] == "COMMON-STOCK":
+            securities = securities.append(line_item, ignore_index=True)
+        elif line_item['SecurityType'] == "CURRENCY":
+            cash_equivalencies= cash_equivalencies.append(line_item, ignore_index=True)
+    grouped_summed_securities = securities.groupby(["SecurityDescription"])[["MarketValue"]].sum().reset_index()
+    consolidated_holdings = cash_equivalencies.drop(columns=["PortfolioId","Quantity", "SecurityCusip", "SecurityMarketPrice", "SecurityType"]).append(grouped_summed_securities)
+
+    return consolidated_holdings
 
 def main():
     begin_run(PROGRAM_NAME)
     config_settings = load_config(PROGRAM_NAME)
-    price_source_id = config_settings['price_source_id']
+    run_date = config_settings['run_date']
+    portfolio_group_name = config_settings['port_group_name']
+    output_file_name = config_settings['output_file_name']
 
-    if not config_settings['run_date_end']:
-        run_date_start = datetime.strptime(config_settings['run_date_start'], '%Y-%m-%d')
-        run_date_end = datetime.date.now()
-    else:
-        run_date_start = datetime.strptime(config_settings['run_date_start'], '%Y-%m-%d')
-
-        run_date_end = datetime.strptime(config_settings['run_date_end'], '%Y-%m-%d')
-
+    group_id = get_port_group_id_from_name(portfolio_group_name)
+    job_id = launch_holdings_report(run_date, group_id)
+    json_result_df = get_export_data_set(job_id)
+    consolidated_holdings = sort_lines_by_security_type(json_result_df)
+    consolidated_holdings.to_excel(output_file_name, sheet_name='Holdings')
     end_run(PROGRAM_NAME)
 
 
